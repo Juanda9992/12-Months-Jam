d768cb6a6358a175ec4a07c73e5764f2affbe549
Juanda9992 <juandavidgonzog1@gmail.com>
2020-10-02 20:09:43 -0500

    Build 1
    Movimiento de personaje
    Movimiento de Camara

---
+++ Ham/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    #region PublicStuff
+    public float moveSpeed;
+    public float smooth;
+    public float jumpForce;
+    #endregion
+
+    #region PrivateStuff
+    private float xMove;
+    private Rigidbody2D rb;
+    #endregion
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {   
+        xMove = Input.GetAxis("Horizontal");
+        if(xMove > 0)
+        {
+            rb.velocity = new Vector2(xMove * moveSpeed, rb.velocity.y);
+        }
+        if(xMove < 0)
+        {
+            rb.velocity = new Vector2(xMove * moveSpeed, rb.velocity.y);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = transform.up * jumpForce * Time.deltaTime * 10;
+        }
+
+    }
+
+    #region Methods
+    #endregion
+}
